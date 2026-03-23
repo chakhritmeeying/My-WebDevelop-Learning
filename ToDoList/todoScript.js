2e77e017873a7135864d32a6eb5a2521c669b71e
chakhritmeeying <74953109+chakhritmeeying@users.noreply.github.com>
2026-03-23 16:47:12 +0700

    Add files via upload

--- ToDoList/todoScript.js
+++ ToDoList/todoScript.js
@@ -1,28 +1,63 @@
-function addTodo() {
+function addList() {
     let input = document.getElementById("todoInput");
     let text = input.value;
 
     if (text.trim() === "") {
-        alert("Please enter a todo item!");
+        alert("Please enter a to-do item")
         input.value = "";
         return;
     }
-
+    // Create li
     let li = document.createElement("li");
-    li.innerText = text;
-
-    li.onclick = function () {
-        this.remove();
+    // Create span for text
+    li.classList.add("todo-item");
+    let span = document.createElement("span");
+    span.innerText = text;
+    // Create button for remove item
+    let removeBtn = document.createElement("button");
+    removeBtn.innerText = "❌";
+    removeBtn.classList.add("button");
+    removeBtn.classList.add("remove-btn");
+    // Press for remove item
+    removeBtn.onclick = function () {
+        li.remove();
     }
+
+    // Click item for line-through
+    span.onclick = function () {
+        if (span.style.textDecoration === "line-through") {
+            span.style.textDecoration = "none";
+        } else {
+            span.style.textDecoration = "line-through";
+        }
+    }
+
+    // Add span + removeBtn in to li
+    li.appendChild(span);
+
+    li.appendChild(removeBtn);
+    // Add li in to ul
     document.getElementById("todoList").appendChild(li);
     input.value = "";
+
+}
+
+
+function clearAll() {
+
+    // document.getElementById("todoList").innerHTML = "";
+    let list = document.getElementById("todoList");
+    if (list.children.length === 0) {
+        alert("ToDo List is empty")
+        return
+    }
+    else {
+        list.innerHTML = "";
+    }
+
 }
 
 
 
-document.getElementById("todoInput").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        addTodo();
-    }
-});
+
 
